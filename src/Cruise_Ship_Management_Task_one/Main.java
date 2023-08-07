8fa385dd1fb92c934ddebc7347f8e37763fffb10
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-08-07 14:00:52 +0530

    initial commit

--- src/Cruise_Ship_Management_Task_one/Main.java
+++ src/Cruise_Ship_Management_Task_one/Main.java
@@ -2,8 +2,11 @@ package Cruise_Ship_Management_Task_one;
 
 public class Main
 {
+    private final String[] cabin_info=new String[12];
+
     public static void main(String[] args)
     {
+        System.out.println("hi");
 
 
     }
