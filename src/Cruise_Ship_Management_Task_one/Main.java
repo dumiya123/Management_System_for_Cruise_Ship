691713a3a2d2938fe6f91d9885102724dad0141b
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-08-07 14:08:47 +0530

    initial commit

--- src/Cruise_Ship_Management_Task_one/Main.java
+++ src/Cruise_Ship_Management_Task_one/Main.java
@@ -1,11 +1,25 @@
 package Cruise_Ship_Management_Task_one;
 
+import java.util.Scanner;
+
 public class Main
 {
     private final String[] cabin_info=new String[12];
 
+    private static final Main cruise_ship_class=new Main();
+
+
     public static void main(String[] args)
     {
+        Scanner sc=new Scanner(System.in);
+
+        for (int i = 0; i <cruise_ship_class.cabin_info.length ; i++)
+        {
+
+
+
+        }
+
 
 
 
