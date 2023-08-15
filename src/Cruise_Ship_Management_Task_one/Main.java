3e0d4fb061bd220e56ce1210f7ffe12281bbecd4
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-08-15 11:16:08 +0530

    initial commit

--- src/Cruise_Ship_Management_Task_one/Main.java
+++ src/Cruise_Ship_Management_Task_one/Main.java
@@ -293,7 +293,7 @@ public class Main
 
     /**
      *OrderAlphabetically method will sort the Customer Names Alphabetically and Print Name
-     *In here i used Bubble sort as the
+     *In here i used Bubble sort as the basic sorting algorithms
      */
 
     public void OrderAlphabetically()
