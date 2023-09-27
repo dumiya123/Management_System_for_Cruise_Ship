b1b802199ab586b30b67e2915ec347428a44d21d
D.I.Gamage <117458336+dumiya123@users.noreply.github.com>
2023-09-27 20:43:57 +0530

    initial commit

--- src/Cruise_Ship_Management_Task_two/Cabin.java
+++ src/Cruise_Ship_Management_Task_two/Cabin.java
@@ -8,40 +8,14 @@ package Cruise_Ship_Management_Task_two;
  */
 public class Cabin
 {
-    private Number_of_passengers_In_the_cabin=0;
+
     private Passenger passenger_one;
     private Passenger passenger_two;
     private Passenger passenger_three;
 
 
 
-    /**
-     * Cabin constructor
-     *
-     */
-    public Cabin()
-    {
-        setPassenger1(new Passenger("N/A","N/A",0.0));                // we are creating a new Passenger object and set it as passenger1.
-        setPassenger2(new Passenger("N/A","N/A",0.0));                // we are creating a new Passenger object and set it as passenger2.
-        setPassenger3(new Passenger("N/A","N/A",0.0));                // we are creating a new Passenger object and set it as passenger3
 
-
-    }
-
-    /**
-     * create getter name called getNumberofPassengersIntheCabin()
-     * getter for numberOfPassengersInTheCabin.
-     * @return number of passengers in the cabin
-     */
-
-    public int getNumberofPassengersIntheCabin()
-    {
-
-
-
-
-
-    }
 
 
 
